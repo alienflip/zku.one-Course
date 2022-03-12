6bf4455e54d12c5e613377ad8dd697f919f6fa91
alienflip <73109076+alienflip@users.noreply.github.com>
2022-03-12 21:26:39 +0000

    Create README.md

--- week_2/semaphore-ui/config/ts/export-config.ts
+++
@@ -1,9 +0,0 @@
-import { config } from './index'
-
-if (require.main === module) {
-    let c = JSON.parse(JSON.stringify(config))
-    if (c.chain.keys) {
-        delete c.chain.keys
-    }
-    console.log(JSON.stringify(c))
-}
--- week_2/semaphore-ui/scripts/serveSnarks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"
-cd ../semaphore/circuits/
-http-server -p 8000 --cors
