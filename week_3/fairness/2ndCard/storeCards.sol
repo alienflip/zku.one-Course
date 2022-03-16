1915b58a8fb849f5506992ff647e9b7220e4b150
alienflip <73109076+alienflip@users.noreply.github.com>
2022-03-16 10:47:19 +0000

    Update storeCards.sol

--- week_3/fairness/2ndCard/storeCards.sol
+++ week_3/fairness/2ndCard/storeCards.sol
@@ -3,13 +3,30 @@ pragma solidity ^0.8.12;
 import "./Verifier.sol";
 
 contract cardGame {
-  function verifyCall(
+
+  struct cardProof {
+    uint256[2] _a,
+    uint256[2][2] _b,
+    uint256[2] _c,
+    uint256[9] _input
+  };
+  
+  cardProofs[] lastCards;
+
+  function verifyNewCompare(
         uint256[2] memory _a,
         uint256[2][2] memory _b,
         uint256[2] memory _c,
         uint256[9] memory _input
   ) public view returns (bool) {
-        require(Verifier.verifyCompare(_a, _b, _c, _input), "Failed compare check");
+        uint256 cardCount = lastCards.length;
+        cardProof newProof;
+        newProof._a = _a;
+        newProof._b = _b;
+        newProof._c = _c;
+        newProof._input = _input;
+        lastCards.push(newProof);
+        require(Verifier.verifyCompareSuite(newProof, lastCard[cardCount - 1]), "Failed compare check");
         return true;
   }
 }
