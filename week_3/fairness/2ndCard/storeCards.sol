9a17093ede1af5b4d8f18500d271ca5cb9d7af0e
alienflip <73109076+alienflip@users.noreply.github.com>
2022-03-16 10:37:10 +0000

    Update storeCards.sol

--- week_3/fairness/2ndCard/storeCards.sol
+++ week_3/fairness/2ndCard/storeCards.sol
@@ -1,1 +1,21 @@
 pragma solidity ^0.8.12;
+
+import "./Verifier.sol";
+
+contract cardGame {
+  address verifier;
+  
+  constructor(address verifierInit) {
+    verifier = verifierInit;
+  }
+  
+  function checkRevealProof(
+        uint256[2] memory _a,
+        uint256[2][2] memory _b,
+        uint256[2] memory _c,
+        uint256[9] memory _input
+  ) public view returns (bool) {
+        require(Verifier.verifyCompare(_a, _b, _c, _input), "Failed reveal pf check");
+        return true;
+  }
+}
