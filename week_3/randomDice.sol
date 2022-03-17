0d3b8ca8e3ec7d710dc3e903f38eaf53f94d44b1
alienflip <73109076+alienflip@users.noreply.github.com>
2022-03-17 17:13:44 +0000

    Update randomDice.sol

--- week_3/randomDice.sol
+++ week_3/randomDice.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.12;
 
 contract randomDice {
-
+  function roll(uint256 randomNumber)
+    external
+    returns(bytes32)
+  {
+    bytes32 block = block.hash;
+    return keccak256(abi.encodePacked(block, randomNumber)) == answer;
+  }
 }
