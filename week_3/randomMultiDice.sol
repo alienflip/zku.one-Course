35e87ad3f70a058965606ac2f4cc2e032b265d2b
alienflip <73109076+alienflip@users.noreply.github.com>
2022-03-17 17:29:58 +0000

    Update randomMultiDice.sol

--- week_3/randomMultiDice.sol
+++ week_3/randomMultiDice.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
+contract randomMultiDice {
+
+  mapping(address => int) membersVoteCount;
+  uint256 memberCallCount = 0;
+  uint256 maxMembers;
+  
+  bytes32 hash;
+
+  contructor(address[] members){
+    for(uint i = 0; i < members.length; i++){
+      members[i] = 0;
+    }
+    maxMembers = members.length;
+  }
+
+  function roll(uint256 randomNumber)
+    external
+    returns(bytes32)
+  {
+  
+    require(membersVoteCount[msg.sender] == 0);
+    require(randomNumber > 100000, "not random enough");
+    
+    bytes32 block = block.hash;
+    hash = abi.encodePacked(hash, randomNumber);
+    
+    if(memberCallCount == maxMembers){
+      return keccak256(abi.encodePacked(hash, block));
+    }
+  }
+}
